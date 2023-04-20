02da7c53c00f73aa92987fd079b1b6f621ecd8ef
Christos <46033926+giahou2000@users.noreply.github.com>
2023-04-20 15:53:18 +0300

    Add files via upload

--- demo.m
+++ demo.m
@@ -4,7 +4,10 @@ import dng2rgb.*
 
 filename = 'RawImage.DNG'; % the file you will import
 [rawim ,XYZ2Cam, wbcoeffs, meta_info] = readdng(filename); % import raw image and its metadata
+% imshow(rawim); % just for testing
 
-
-bayertype = 'GRBG'; % the pattern of the sensor's pixels
+bayertype = 'RGGB'; % the pattern of the sensor's pixels (BGGR, GBRG, GRBG, RGGB)
+% method = 'nearest';
+method = 'linear';
+[M, N] = size(rawim); % N is width and M is height
 [Csrgb, Clinear, Cxyz, Ccam] = dng2rgb(rawim, XYZ2Cam, wbcoeffs, bayertype, method, M, N);
